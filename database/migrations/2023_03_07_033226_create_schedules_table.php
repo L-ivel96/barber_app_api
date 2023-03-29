9ad9aa77e9d7c5071310ac4446907e3f84f0ec40
Levi <levi.siqueira@outlook.com>
2023-03-29 12:40:07 -0300

    feat: commit inicial com API funcional com as rotas das entidades base do sistema

---
+++ database/migrations/2023_03_07_033226_create_schedules_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Models\Employee;
+use App\Models\Service;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->id();
+            $table->date('scheduling_date')->nullable();
+            $table->time('hour_start')->nullable();
+            $table->time('hour_end')->nullable();
+            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Employee::class);
+            $table->foreignIdFor(Service::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+}
